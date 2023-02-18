3e6915631c935d73dcdd3930e957cc96f6d7f0e6
lby132 <quddbscjswow@naver.com>
2023-02-19 03:01:09 +0900

    react board front

---
+++ src/pages/MainPage.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Main = () => {
+    return (
+        <div>
+            <table>
+                <thead>
+                <tr>
+                    <th>id</th>
+                    <th>title</th>
+                    <th>auth</th>
+                    <th>category</th>
+                    <th>content</th>
+                    <th>cnt</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
