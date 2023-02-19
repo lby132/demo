a0e67062478af2ca40fecd932caab5604f823d30
lby132 <quddbscjswow@naver.com>
2023-02-20 00:01:25 +0900

    log level, cors 설정

--- src/test/java/com/example/demo/service/BoardServiceTest.java
+++ src/test/java/com/example/demo/service/BoardServiceTest.java
@@ -3,21 +3,20 @@ package com.example.demo.service;
 import com.example.demo.domain.Board;
 import com.example.demo.request.PostCreate;
 import com.example.demo.response.PostResponse;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
 
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
 @SpringBootTest
 @Transactional
+@Rollback(value = false)
 class BoardServiceTest {
 
     @Autowired
@@ -30,19 +29,19 @@ class BoardServiceTest {
     void boardSaveTest() {
         //given
         PostCreate board = PostCreate.builder()
-                .title("title")
-                .category("study")
-                .content("hi")
+                .title("title1")
+                .category("study1")
+                .content("hi1")
                 .build();
 
         //when
         boardService.boardSave(board);
 
         //then
-        assertEquals(1L, boardRepository.count());
+        assertEquals(2L, boardRepository.count());
         Board post = boardRepository.findAll().get(0);
-        assertEquals("title", post.getTitle());
-        assertEquals("hi", post.getContent());
+        assertEquals("title1", post.getTitle());
+        assertEquals("hi1", post.getContent());
 
     }
 
